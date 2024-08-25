aa6bd89140e635151912a95ba42c203496595423
miadityaingale <138990062+miadityaingale@users.noreply.github.com>
2024-08-25 19:43:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const cors = require('cors');
 
 const app = express();
 
-// app.use(cors({
-// }));
-
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
